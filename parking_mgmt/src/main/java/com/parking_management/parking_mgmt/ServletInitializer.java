54603ca530d3399844f9bf684bad17801e8e9e4c
anurag mishra <119682076+anurag2801@users.noreply.github.com>
2023-02-22 21:02:27 +0530

    spring code

---
+++ parking_mgmt/src/main/java/com/parking_management/parking_mgmt/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.parking_management.parking_mgmt;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ParkingMgmtApplication.class);
+	}
+
+}
---
+++ parking_mgmt/src/test/java/com/parking_management/parking_mgmt/ParkingMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking_management.parking_mgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
