921f4e49735b7e6ebfc7efe5a547534d676d8c77
Ishaan Kadam <ishaankad@gmail.com>
2023-02-24 16:31:41 +0530

    Eclipse project added to repo

---
+++ eclipse-parking_mgmt/ParkingManagement/src/test/java/com/CdacProject/ParkingManagement/ParkingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CdacProject.ParkingManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
